bb8774c5b13fe3df4fe4b3407182ca02dd3d0a16
Andy Loftus <aloftus@gmail.com>
2019-07-19 15:10:33 +0000

    Update sample structure to match IT-1141

--- mk_example.sh
+++ mk_example.sh
@@ -1,47 +1,50 @@
-DATACENTERS=( ncsa_npcf ncsa_3003 tucson bdc summit )
-CLUSTERS=( k8s_prod k8s_test comcam auxtel lsstcam )
+DATACENTERS=( npcf ncsa3003 tucson bdc summit )
+CLUSTERS=( oracle k8s_prod k8s_test comcam auxtel lsstcam )
 ROLES=( default )
+SITES=( ncsa tucson laserena cerrapachon )
+TYPES=( virtual default )
 
 mkyaml() {
     echo "---" >"${1}.yaml"
 }
 
-mkdir 10node
+rm -rf datacenter cluster role node
 
 for dc in "${DATACENTERS[@]}"; do
+    mkdir -p datacenter
+    mkyaml datacenter/$dc
 
     for cl in "${CLUSTERS[@]}"; do
+        mkdir -p datacenter/$dc/cluster
+        mkyaml datacenter/$dc/cluster/$cl
+        mkdir -p cluster
+        mkyaml cluster/$cl
 
         for rl in "${ROLES[@]}"; do
+            mkdir -p datacenter/$dc/cluster/$cl/role
+            mkyaml datacenter/$dc/cluster/$cl/role/$rl
+            mkdir -p datacenter/$dc/role
+            mkyaml datacenter/$dc/role/$rl
+            mkdir -p cluster/$cl/role
+            mkyaml cluster/$cl/role/$rl
+            mkdir -p role
+            mkyaml role/$rl
 
-            _20="20datacenter_cluster_role/$dc/$cl"
-            mkdir -p "$_20"
-            mkyaml "$_20/$rl"
-
-            _30="30datacenter_role"
-            mkdir -p "$_30/$dc"
-            mkyaml "$_30/$rl"
-
-            _50="50role"
-            mkdir -p "$_50"
-            mkyaml "$_50/$rl"
-
+            for typ in "${TYPES[@]}"; do
+                mkdir -p role/$rl/type
+                mkyaml role/$rl/type/$typ
+                mkdir -p type
+                mkyaml type/$typ
+            done
         done
-
-        _40="40datacenter_cluster/$dc"
-        mkdir -p "$_40"
-        mkyaml "$_40/$cl"
-
-        _60="60cluster"
-        mkdir -p "$_60"
-        mkyaml "$_60/$cl"
-
     done
-
-    _70="70datacenter"
-    mkdir -p "$_70"
-    mkyaml "$_70/$dc"
-
 done
 
-mkyaml "80common"
+mkdir -p site
+for s in "${SITES[@]}"; do
+    mkyaml site/$s
+done
+
+mkyaml common
+mkdir -p node
+mkyaml node/puppet.internal
