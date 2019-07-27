09049099f14848556274f1c376d6326b67b1bf05
Andy Loftus <aloftus@gmail.com>
2019-07-27 02:38:28 +0000

    Remove datacenter, type
    Add node yaml for agent-centos-1.internal

--- mk_example.sh
+++ mk_example.sh
@@ -1,50 +1,44 @@
-DATACENTERS=( npcf ncsa3003 tucson bdc summit )
 CLUSTERS=( oracle k8s_prod k8s_test comcam auxtel lsstcam )
 ROLES=( default )
-SITES=( ncsa tucson laserena cerrapachon )
-TYPES=( virtual default )
+SITES=( npcf ncsa tucson bdc summit )
 
 mkyaml() {
     echo "---" >"${1}.yaml"
 }
 
-rm -rf datacenter cluster role node
+rm -rf site cluster role node
 
-for dc in "${DATACENTERS[@]}"; do
-    mkdir -p datacenter
-    mkyaml datacenter/$dc
+for si in "${SITES[@]}"; do
+    mkdir -p site
+    mkyaml site/$si
 
     for cl in "${CLUSTERS[@]}"; do
-        mkdir -p datacenter/$dc/cluster
-        mkyaml datacenter/$dc/cluster/$cl
+        mkdir -p site/$si/cluster
+        mkyaml site/$si/cluster/$cl
         mkdir -p cluster
         mkyaml cluster/$cl
 
         for rl in "${ROLES[@]}"; do
-            mkdir -p datacenter/$dc/cluster/$cl/role
-            mkyaml datacenter/$dc/cluster/$cl/role/$rl
-            mkdir -p datacenter/$dc/role
-            mkyaml datacenter/$dc/role/$rl
+            mkdir -p site/$si/cluster/$cl/role
+            mkyaml site/$si/cluster/$cl/role/$rl
+            mkdir -p site/$si/role
+            mkyaml site/$si/role/$rl
             mkdir -p cluster/$cl/role
             mkyaml cluster/$cl/role/$rl
             mkdir -p role
             mkyaml role/$rl
 
-            for typ in "${TYPES[@]}"; do
-                mkdir -p role/$rl/type
-                mkyaml role/$rl/type/$typ
-                mkdir -p type
-                mkyaml type/$typ
-            done
         done
     done
-done
-
-mkdir -p site
-for s in "${SITES[@]}"; do
-    mkyaml site/$s
 done
 
 mkyaml common
 mkdir -p node
 mkyaml node/puppet.internal
+
+# Add classes to default role
+>role/default.yaml cat <<ENDHERE
+---
+classes:
+  - profile::baseline_cfg
+ENDHERE
